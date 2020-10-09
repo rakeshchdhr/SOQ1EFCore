b56c09223bf2c3a1fd14905822322d689ba0e2ce
Rakesh Chaudhari <rakeshc@centiro.se>
2020-10-09 22:28:23 +0530

    Added Solution

---
+++ SOTest2/SOTest2/Migrations/20201009164047_InitialCreate.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SOTest2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Service",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Service", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceReviews",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceId = table.Column<long>(nullable: false),
+                    ReviewId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceReviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiceReviews_Review_ReviewId",
+                        column: x => x.ReviewId,
+                        principalTable: "Review",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ServiceReviews_Service_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Service",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceReviews_ReviewId",
+                table: "ServiceReviews",
+                column: "ReviewId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceReviews_ServiceId",
+                table: "ServiceReviews",
+                column: "ServiceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServiceReviews");
+
+            migrationBuilder.DropTable(
+                name: "Review");
+
+            migrationBuilder.DropTable(
+                name: "Service");
+        }
+    }
+}
---
+++ SOTest2/SOTest2/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SOTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SOTestDBContext())
+            {
+                var serviceReview = new ServiceReview()
+                {
+                    Review = new Review()
+                    {
+                        Name = "TestReivew",
+                        ServiceReviews = null
+                    },
+                    Service = new Service()
+                    {
+                        Name = "TestService",
+                        ServiceReviews = null
+                    }
+                };
+                db.ServiceReviews.Add(serviceReview);
+                db.SaveChanges();
+            }
+
+            Console.WriteLine("Done !");
+        }
+    }
+
+    public class SOTestDBContext : DbContext
+    {
+        public DbSet<Review> Review { get; set; }
+        public DbSet<Service> Service { get; set; }
+        public DbSet<ServiceReview> ServiceReviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=.;Database=SOTestDB;Integrated Security=True");
+        }
+    }
+
+    public class Service
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<ServiceReview> ServiceReviews { get; set; } = new List<ServiceReview>();
+    }
+    public class Review
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<ServiceReview> ServiceReviews { get; set; } = new List<ServiceReview>();
+    }
+
+    public class ServiceReview
+    {
+        [Key]
+        public long Id { get; set; }
+        public long ServiceId { get; set; }
+        [ForeignKey("ServiceId")]
+        public virtual Service Service { get; set; } = new Service();
+        public long ReviewId { get; set; }
+        [ForeignKey("ReviewId")]
+        public virtual Review Review { get; set; } = new Review();
+    }
+}
